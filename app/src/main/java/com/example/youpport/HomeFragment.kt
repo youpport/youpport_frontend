6d0ee2cdfe228546ee87fd755827c5b4f45ee510
Sohyunnnn <thgus05061@naver.com>
2023-12-29 03:22:42 +0900

    [Feat] 지원사업 Fragment UI 구현 완료 및 화면 연결

--- app/src/main/java/com/example/youpport/HomeFragment.kt
+++ app/src/main/java/com/example/youpport/HomeFragment.kt
@@ -1,59 +1,39 @@
 package com.example.youpport
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
+import androidx.databinding.DataBindingUtil.setContentView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        // findViewById 대신 view.findViewById 사용
+        val btnAll = view.findViewById<AppCompatButton>(R.id.btn_all)
+
+        if (btnAll != null) {
+            btnAll.setOnClickListener {
+                requireActivity().supportFragmentManager.beginTransaction()
+                    .replace(
+                        R.id.HomeFragment,
+                        SupportProjectFragment()
+                    )
+                    .commit()
             }
+        }
+
+        return view
     }
 }
+
+
